927123c56c52715f494461d9e661cbaa2a5f95cf
Usman <oets.man@gmail.com>
2023-02-21 10:56:01 +0700

    cls masih tinggi

--- src/scripts/pages/favorite.js
+++ src/scripts/pages/favorite.js
@@ -3,26 +3,6 @@ import dataIdb from '../data/restaurants-idb';
 
 const Favorite = {
 	async renderHeader() {
-		const hero = document.querySelector('#hero');
-		hero.innerHTML = `
-			<picture>
-				<source type="image/webp" media="(max-width: 480px)" srcset="./images-sm/hero.webp">
-				<source type="image/jpeg" media="(max-width: 480px)" srcset="./images-sm/hero.jpg">
-
-				<source type="image/webp" media="(max-width: 768px)" srcset="./images-md/hero.webp">
-				<source type="image/jpeg" media="(max-width: 768px)" srcset="./images-md/hero.jpg">
-
-				<source type="image/webp" srcset="./images-lg/hero.webp">
-				<source type="image/jpeg" srcset="./images-lg/hero.jpg">
-
-				<img src="./images-lg/hero.jpg" alt="hero" id="hero-img" />
-			</picture>
-			<div class="hero-content" id="hero-content">
-				<p class="hero-title">Cita Rasa</p>
-				<p class="hero-subtitle">Kuliner Nusantara</p>
-			</div>
-		`;
-
 		const main = document.querySelector('#main');
 		main.innerHTML = `
             <h2>Daftar Warung Favorit</h2>
